5b93aa594187d1162b85e81f3c2cf07e67563ab4
asayah <adam.sayah@solo.io>
2020-12-01 12:35:24 -0700

    waf

---
+++ util/prepare-directories.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+mkdir -p ./data/artifact/artifacts/gloo-system
+mkdir -p ./data/config/{gateways,proxies,upstreams,upstreamgroups,routetables,authconfigs,virtualservices}/gloo-system
+mkdir -p ./data/secret/secrets/{default,gloo-system}
+
